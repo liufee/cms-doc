9f4e42742a40b05a35a538c04bd4810259f2b2a2
liufee <job@feehi.com>
2025-08-22 08:28:23 +0800

    feat: baidu tongji

--- clean.js
+++ clean.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
+const baiduTongji = `<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a8620f6cad15d24a9fa7554e3496225f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+`;
+
 function walk(dir) {
     for (const file of fs.readdirSync(dir)) {
         const fullPath = path.join(dir, file);
@@ -10,6 +21,7 @@ function walk(dir) {
         } else if (file.endsWith('.html')) {
             let content = fs.readFileSync(fullPath, 'utf8');
             content = content.replace(/<li>\s*<a[^>]*class=["']gitbook-link["'][\s\S]*?<\/a>\s*<\/li>/gi, '');
+            content = content.replace('</head>', baiduTongji + '\n</head>');
             fs.writeFileSync(fullPath, content);
         }
     }
